784e70959f4baef26e0c84cdcd45f883c208feea
dac5190 <dac5190@gmail.com>
2024-12-31 09:53:22 -0500

    updates to the script to make it remove the docker container more aggressively and cmd friendly.

--- runDockerFileNCompile.bat
+++ runDockerFileNCompile.bat
@@ -1,6 +1,10 @@
 @echo off
+echo Stopping current images running older version
+for /f "tokens=*" %%i in ('docker ps -a -f "ancestor=my-gnucobol-image" -q') do docker stop %%i
+
 echo Removing existing image...
-docker image rm -f my-gnucobol-image 2>nul 
+#docker image rm -f my-gnucobol-image 2>nul 
+for /f "tokens=*" %%i in ('docker ps -a -f "ancestor=my-gnucobol-image" -q') do docker container rm %%i -f
 
 echo removing any dangling images
 docker images -f "dangling=true" -q | xargs docker rmi -f
